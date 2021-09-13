a8fa896e74e7941fc2117851d2768ce921d4372d
BeeverFeever <scottyadam6@gmail.com>
2021-09-13 17:30:06 +1000

    Bug still in julia version

--- Tic_Tac_Toe-Julia/app.jl
+++ Tic_Tac_Toe-Julia/app.jl
@@ -1,6 +1,4 @@
-board = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
-
-function GenerateBoard()
+function PrintBoard(board)
   println("$(board[1])|$(board[2])|$(board[3])
 $(board[3])|$(board[4])|$(board[5]) 
 $(board[7])|$(board[8])|$(board[9])")
@@ -9,11 +7,23 @@ end
 function GetInput(player, _board)
   print("Player $(player), select where to place your piece 1-9: ")
   place = parse(Int, readline())
-  if player == 'X'
-    _board[place] = player
-  end
-  if player == 'O'
-    _board[place] = player
+  
+  run = true 
+
+  while run
+    if _board[place] == '-' && 1 < place < 9
+      if player == 'X'
+        _board[place] = player
+        run = false 
+      end
+      if player == 'O'
+        _board[place] = player
+        run = false 
+      end
+    else
+      println("Please select a valid place: ")
+      place = parse(Int, readline())
+    end
   end
 
   return _board
@@ -21,47 +31,57 @@ end
 
 function CheckWin(piece, _board)
   #check vertical wins
-  for i in range(3)
+  for i in 1:3
     if _board[i] == piece && _board[i + 3] == piece && _board[i + 6] == piece
       winner = piece
-
-  #check horizontal wins
-  for i in range(0, 8, step = 4)
-    if _board[i] == piece && _board[i + 1] == piece && _board[i + 2] == piece
-      winner = piece
-    
-  #check diagonal wins
-  if _board[0] == piece && _board[4] == piece && _board[8] == piece
-    winner = piece
-
-  if _board[i] == piece && _board[2] == piece && _board[4] == piece
-    winner = piece
-  
-  #check tie
-  for i in range(len(_board))
-    if _board[i] != 'X' || 'O'
-      return
-    else if i == 8
-      winner = "Tie"
-
-  return winner
-
-function MainGameLoop()
-  playing = true 
-
-  while playing == true 
-    GenerateBoard()
-    board = GetInput('X', board)
-    winner = CheckWin('X', board)
-    winner = CheckWin('O', board)
-
-    GenerateBoard()
-    board = GetInput('O', board)
-    winner = CheckWin('X', board)
-    winner = CheckWin('O', board)
+    end
   end
 
-  if winner == 'X' || 'O'
+  #check horizontal wins
+  for i in 1:3:7
+    if _board[i] == piece && _board[i + 1] == piece && _board[i + 2] == piece~
+      winner = piece
+    end
+  end
+
+  #check diagonal wins
+  if _board[1] == piece && _board[5] == piece && _board[9] == piece
+    winner = piece
+  end
+
+  if _board[3] == piece && _board[5] == piece && _board[7] == piece
+    winner = piece
+  end
+
+  #check tie
+  for i in 1:(length(_board) - 1)
+    if _board[i] != 'X' || _board[i] != 'O'
+      return
+    elseif i == 9
+      winner = "Tie"
+    end
+  end
+
+  return winner
+end  
+
+function MainGameLoop()
+  board = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
+  playing = true 
+
+  while playing 
+    println("testing")
+
+    PrintBoard(board[])
+    board[] = GetInput('X', board[])
+    winner = CheckWin('X', board[])
+    winner = CheckWin('O', board[])
+
+
+    println("alskdhfasdklf")
+  end
+
+  if winner == 'X' || winner == 'O'
     prinln("$winner is the winner!")
     playing = false
   end
