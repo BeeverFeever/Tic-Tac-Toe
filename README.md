e1acf599c546eb082ebfd69b3f9a465c594a97da
BeeverFeever <scottyadam6@gmail.com>
2021-09-13 13:12:58 +1000

    Almost finished Julia version.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Tic-Tac-Toe
+
+Very basic Tic Tac Toe games made in langages that I learn.
--- Tic_Tac_Toe-Julia/app.jl
+++ Tic_Tac_Toe-Julia/app.jl
@@ -1,5 +1,4 @@
 board = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
-playing = true
 
 function GenerateBoard()
   println("$(board[1])|$(board[2])|$(board[3])
@@ -7,23 +6,69 @@ $(board[3])|$(board[4])|$(board[5])
 $(board[7])|$(board[8])|$(board[9])")
 end
 
-function GetInput(player = ' ')
+function GetInput(player, _board)
   print("Player $(player), select where to place your piece 1-9: ")
-  place = readline()
+  place = parse(Int, readline())
   if player == 'X'
-    board[place] = player
+    _board[place] = player
   end
   if player == 'O'
-    print("Place $place. Player $player.")
-    board[place] = player
-    println("test 2")
+    _board[place] = player
+  end
+
+  return _board
+end
+
+function CheckWin(piece, _board)
+  #check vertical wins
+  for i in range(3)
+    if _board[i] == piece && _board[i + 3] == piece && _board[i + 6] == piece
+      winner = piece
+
+  #check horizontal wins
+  for i in range(0, 8, step = 4)
+    if _board[i] == piece && _board[i + 1] == piece && _board[i + 2] == piece
+      winner = piece
+    
+  #check diagonal wins
+  if _board[0] == piece && _board[4] == piece && _board[8] == piece
+    winner = piece
+
+  if _board[i] == piece && _board[2] == piece && _board[4] == piece
+    winner = piece
+  
+  #check tie
+  for i in range(len(_board))
+    if _board[i] != 'X' || 'O'
+      return
+    else if i == 8
+      winner = "Tie"
+
+  return winner
+
+function Main()
+  playing = true 
+
+  while playing == true 
+    GenerateBoard()
+    board = GetInput('X', board)
+    winner = CheckWin('X', board)
+    winner = CheckWin('O', board)
+
+    GenerateBoard()
+    board = GetInput('O', board)
+    winner = CheckWin('X', board)
+    winner = CheckWin('O', board)
+  end
+
+  if winner == 'X' || 'O'
+    prinln("$winner is the winner!")
+    playing = false
+  end
+  if winner == "Tie"
+    prinln("It is a tie")
+    playing = false 
   end
 end
 
-function MainGameLoop()
-  GenerateBoard()
-  GetInput('O')
-  GenerateBoard()
-end
-
-MainGameLoop()
+Main()
