0a971dab262fd4c4d920aafca3461928813bf5aa
BeeverFeever <scottyadam6@gmail.com>
2021-09-16 17:51:23 +1000

    Finished julia version. Problem when running in repl in vscode

--- Tic_Tac_Toe-Julia/app.jl
+++ Tic_Tac_Toe-Julia/app.jl
@@ -1,31 +1,24 @@
-function PrintBoard(board)
-  println("$(board[1])|$(board[2])|$(board[3])
-$(board[3])|$(board[4])|$(board[5]) 
-$(board[7])|$(board[8])|$(board[9])")
+function PrintBoard(_board)
+  println("$(_board[1])|$(_board[2])|$(_board[3])\n$(_board[4])|$(_board[5])|$(_board[6])\n$(_board[7])|$(_board[8])|$(_board[9])")
 end
 
 function GetInput(player, _board)
   print("Player $(player), select where to place your piece 1-9: ")
-  place = parse(Int, readline())
-  
-  run = true 
 
-  while run
-    if _board[place] == '-' && 1 < place < 9
-      if player == 'X'
-        _board[place] = player
-        run = false 
-      end
-      if player == 'O'
-        _board[place] = player
-        run = false 
-      end
-    else
-      println("Please select a valid place: ")
-      place = parse(Int, readline())
-    end
+  @label Start
+  place = readline()
+
+  if place ∉ String["$i" for i in 1:9]
+    @goto InvalidEntry
+  end
+  num = parse(Int, place)
+  if _board[num] != '-'
+    @label InvalidEntry
+    print("Please select a valid place: ")
+    @goto Start
   end
 
+  _board[num] = player
   return _board
 end
 
@@ -33,61 +26,60 @@ function CheckWin(piece, _board)
   #check vertical wins
   for i in 1:3
     if _board[i] == piece && _board[i + 3] == piece && _board[i + 6] == piece
-      winner = piece
+      return piece
     end
   end
 
   #check horizontal wins
   for i in 1:3:7
-    if _board[i] == piece && _board[i + 1] == piece && _board[i + 2] == piece~
-      winner = piece
+    if _board[i] == piece && _board[i + 1] == piece && _board[i + 2] == piece
+      return piece
     end
   end
 
   #check diagonal wins
   if _board[1] == piece && _board[5] == piece && _board[9] == piece
-    winner = piece
+    return piece
   end
 
   if _board[3] == piece && _board[5] == piece && _board[7] == piece
-    winner = piece
+    return piece
   end
 
-  #check tie
-  for i in 1:(length(_board) - 1)
-    if _board[i] != 'X' || _board[i] != 'O'
-      return
-    elseif i == 9
-      winner = "Tie"
+  #checks for tie
+  for i in 1:9
+    if _board[i] == '-'
+      return 'n'
     end
   end
 
-  return winner
+  return 't'
 end  
 
 function MainGameLoop()
-  board = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
-  playing = true 
+  board = ['-', '-', '-', '-', '-', '-', '-', '-', '-',]
+  turn = true
 
-  while playing 
-    println("testing")
+  while true
+    PrintBoard(board)
 
-    PrintBoard(board[])
-    board[] = GetInput('X', board[])
-    winner = CheckWin('X', board[])
-    winner = CheckWin('O', board[])
+    turn ? player = 'X' : player = 'O'
+    board = GetInput(player, board)
 
+    check = CheckWin(player, board)
 
-    println("alskdhfasdklf")
-  end
-
-  if winner == 'X' || winner == 'O'
-    prinln("$winner is the winner!")
-    playing = false
-  end
-  if winner == "Tie"
-    prinln("It is a tie")
-    playing = false 
+    if check != 'n'
+      if check == 't'
+        PrintBoard(board)
+        println("The game is a Tie!")
+        return
+      else 
+        PrintBoard(board)
+        println("Play $player has won!")
+        return
+      end
+    end
+    turn = !turn
   end
 end
 
